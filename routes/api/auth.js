ca1381a5929be32e566624841e4499ebc393e055
tomkent321 <tomkent321@yahoo.com>
2019-05-11 15:47:14 -0600

    auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const User = require('../../models/User');
+const { check, validationResult } = require('express-validator/check');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const bcrypt = require('bcryptjs');
 
 // @route   GET api/auth
 // @desc    Test route
@@ -16,5 +20,53 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Sever error');
   }
 });
+
+// @route   POST api/auth
+// @desc   Authenticate user & get token
+// @access  Public
+router.post(
+  '/',
+  [check('password', 'Password is required').exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      //See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid credentials' }] });
+      }
+
+      //for jwt:
+      const payload = {
+        user: {
+          // the user.save() above returns a promise with the id
+          //mongoose abstracts the underscore:  id: user._id can use:
+          id: user._id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
